a443cc80f97ee39711f166f5240a54216b5bba51
mbeuil <mbeuil@student.42.fr>
2022-11-17 15:37:46 +0200

    chore: add recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
 import '../styles/globals.css';
+
+import * as React from 'react';
+
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <RecoilRoot>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RecoilRoot>
     </React.StrictMode>
   );
 }
